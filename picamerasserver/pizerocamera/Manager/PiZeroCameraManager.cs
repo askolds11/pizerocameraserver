64176f52637c62f714402a3de57dda6ea2d321c3
askolds <askolds11@gmail.com>
2025-07-30 10:38:46 +0300

    Add cameras to database in manager

--- picamerasserver/pizerocamera/Manager/PiZeroCameraManager.cs
+++ picamerasserver/pizerocamera/Manager/PiZeroCameraManager.cs
@@ -1,5 +1,8 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using MQTTnet;
+using picamerasserver.Database;
+using picamerasserver.Database.Models;
 using picamerasserver.Options;
 
 namespace picamerasserver.pizerocamera.manager;
@@ -12,14 +15,19 @@ public partial class PiZeroCameraManager
 
     public readonly IReadOnlyList<string> PiZeroCameraIds;
     public readonly IReadOnlyDictionary<string, PiZeroCamera> PiZeroCameras;
+    private readonly IDbContextFactory<PiDbContext> _dbContextFactory;
     public event Action? OnChange;
 
     public PiZeroCameraManager(IMqttClient mqttClient, ILogger<PiZeroCameraManager> logger,
-        IOptionsMonitor<MqttOptions> optionsMonitor)
+        IOptionsMonitor<MqttOptions> optionsMonitor, IDbContextFactory<PiDbContext> dbContextFactory)
     {
         _mqttClient = mqttClient;
         _logger = logger;
         _optionsMonitor = optionsMonitor;
+        _dbContextFactory = dbContextFactory;
+
+        using var piDbContext = _dbContextFactory.CreateDbContext();
+        
         // Add all cameras
         var piZeroCamerasIds = new List<string>();
         var piZeroCameras = new Dictionary<string, PiZeroCamera>();
@@ -30,8 +38,14 @@ public partial class PiZeroCameraManager
                 var id = letter + number;
                 piZeroCamerasIds.Add(id);
                 piZeroCameras.Add(id, new PiZeroCamera { Id = id });
+
+                if (!piDbContext.Cameras.Any(x => x.Id == id))
+                {
+                    piDbContext.Add(new CameraModel { Id = id });
+                }
             }
         }
+        piDbContext.SaveChanges();
 
         PiZeroCameraIds = piZeroCamerasIds;
         PiZeroCameras = piZeroCameras;
