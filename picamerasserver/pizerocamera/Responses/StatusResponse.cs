b5a36a8874d5b1c096782c7be5e538f769cc9195
askolds <askolds11@gmail.com>
2025-08-15 14:20:42 +0300

    Cleanup, page title fix

--- picamerasserver/pizerocamera/Responses/StatusResponse.cs
+++ picamerasserver/pizerocamera/Responses/StatusResponse.cs
@@ -1,4 +1,1 @@
 global using StatusResponse = picamerasserver.pizerocamera.Responses.SuccessWrapper<picamerasserver.pizerocamera.PiZeroCameraStatus>;
-
-namespace picamerasserver.pizerocamera.Responses;
-
